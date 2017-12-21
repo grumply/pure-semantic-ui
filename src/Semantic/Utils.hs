d81c6b04a7e1423ebf9652f1e96b7490ba3a218c
grumply <sean@grump.ly>
2017-12-21 00:26:47 -0600

    Helper utils.
    #!, #?, and #!? are candidates for Pure.Data.Cond inclusion

--- src/Semantic/Utils.hs
+++ src/Semantic/Utils.hs
@@ -1,6 +1,9 @@
-module Semantic.Utils where
+module Semantic.Utils (module Semantic.Utils, module Export) where
 
 import Pure.Data
+import Pure.View ((<<>>))
+
+import Data.Function as Export
 
 data TextAlignment = AlignedLeft | AlignedCenter | AlignedRight | AlignedJustified deriving (Eq,Ord,Generic,Default)
 
@@ -10,3 +13,21 @@ textAlignClass (Just ta) =
     AlignedCenter -> "center aligned"
     AlignedRight -> "right aligned"
     AlignedJustified -> "justified"
+
+useKeyOrValueAndKey val key =
+  maybe "" (<<>> key) val
+
+oneEq :: Eq a => a -> a -> a -> Maybe a
+oneEq l r x = if l == x then Just l else if r == x then Just r else Nothing
+
+-- if x is nil then y else nil
+(#!) :: (Cond x, Default a) => x -> a -> a
+(#!) x y = (isNil x) # y
+
+-- if x is (Just non-nil) then y else nil
+(#?) :: (Cond x, Default a, Cond a) => Maybe x -> a -> a
+(#?) x y = may (# y) x
+
+-- if x is (Just nil) then y else nil
+(#!?) :: (Cond x, Default a, Cond a) => Maybe x -> a -> a
+(#!?) x y = may (#! y) x
