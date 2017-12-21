9b0d8f94546d5d057cb13017fef53d3c85f1eb02
grumply <sean@grump.ly>
2017-12-21 00:27:37 -0600

    Implement Label. Closes #4

---
+++ src/Semantic/Elements/Label.hs
@@ -0,0 +1,72 @@
+module Semantic.Elements.Label where
+
+import GHC.Generics as G
+import Pure.View as View
+
+import Semantic.Utils
+
+data Label ms = Label_
+    { as :: [Feature ms] -> [View ms] -> View ms
+    , active :: Bool
+    , attached :: Txt
+    , attributes :: [Feature ms]
+    , basic :: Bool
+    , children :: [View ms]
+    , circular :: Bool
+    , classes :: [Txt]
+    , color :: Txt
+    , corner :: Maybe Txt
+    , empty :: Bool
+    , floating :: Bool
+    , horizontal :: Bool
+    , image :: Maybe (View ms)
+    , handleClick :: Ef ms IO ()
+    , pointing :: Txt
+    , ribbon :: Maybe Txt
+    , size :: Txt
+    , tag :: Bool
+    } deriving (Generic)
+
+instance Default (Label ms) where
+    def = (G.to gdef) { as = View.Label }
+
+pattern Label :: Typeable ms => Label ms -> View ms
+pattern Label l = View l
+
+instance Typeable ms => Pure Label ms where
+    render Label_ {..} =
+        let
+            pointingClass =
+                -- note the careful class ordering
+                ($ "pointing") $ flip (maybe (const nil)) pointing $ \case
+                    (oneEq "left" "right"  -> Just lr) -> (lr <<>>)
+                    (oneEq "above" "below" -> Just ab) -> (<<>> ab)
+                    "" -> id
+                    _  -> const nil
+            
+            cs =
+                ( "ui"
+                : color
+                : pointingClass
+                : size
+                : active # "active"
+                : basic # "basic"
+                : circular # "circular"
+                : empty # "empty"
+                : floating # "floating"
+                : horizontal # "horizontal"
+                : image #!? "image"
+                : tag # "tag"
+                : corner # "corner"
+                : ribbon # "ribbon"
+                : attached # "attached"
+                : "label"
+                : classes
+                )
+        in
+            as 
+                ( ClassList cs
+                : handleClick # onClick handleClick
+                : attributes
+                )
+                children
