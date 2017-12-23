cd211ce6a446b250338cb8f8600ac3dde429e28a
grumply <sean@grump.ly>
2017-12-23 12:38:59 -0600

    Clean up exports.

--- src/Semantic.hs
+++ src/Semantic.hs
@@ -3,5 +3,6 @@ module Semantic (module Export) where
 import Data.Function as Export
 import Semantic.Elements as Export
 import Semantic.Extensions as Export
+import Semantic.Utils as Export
 
-import Pure.View as Export hiding (name,Label,Button,(!),(%),Name,Size,Big,Small,Color,Disabled,Link,Empty,Hidden) 
+import Pure.View as Export hiding (name,Label,Button,(!),(%),Name,Size,Big,Small,Color,Disabled,Link,Empty,Hidden,one,two,Width,Scale) 
