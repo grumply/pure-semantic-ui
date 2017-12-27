f55b6dd048e14330d024fa1aa7ea6cc5ef3eb861
grumply <sean@grump.ly>
2017-12-26 23:14:13 -0600

    Export properties for #68, #67, and #64.

--- src/Semantic/Properties.hs
+++ src/Semantic/Properties.hs
@@ -1,11 +1,14 @@
+module Semantic.Properties (module Semantic.Properties, module Export) where
 
 import Semantic.Properties.Active as Export
 import Semantic.Properties.Animated as Export
 import Semantic.Properties.As as Export
+import Semantic.Properties.Attached as Export
 import Semantic.Properties.Attributes as Export
 import Semantic.Properties.Avatar as Export
 import Semantic.Properties.Basic as Export
-import Semantic.Properties.Basic as Export
+import Semantic.Properties.Block as Export
+import Semantic.Properties.Bordered as Export
 import Semantic.Properties.Bulleted as Export
 import Semantic.Properties.Children as Export
 import Semantic.Properties.Classes as Export
