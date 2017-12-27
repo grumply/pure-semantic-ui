7961d260c1ebc66693108678f6ad9b063f71343a
grumply <sean@grump.ly>
2017-12-26 18:26:49 -0600

    Add HasAttributes instance for View. Closes #60.

--- src/Semantic/Extensions/Attributes.hs
+++ src/Semantic/Extensions/Attributes.hs
@@ -1,5 +1,7 @@
 module Semantic.Extensions.Attributes where
 
+import Pure.View
+
 import Semantic.Extensions.Utils
 
 class HasAttributes a where
@@ -10,7 +12,27 @@ class HasAttributes a where
 pattern Attributes :: HasAttributes a => [Attribute a] -> a -> a
 pattern Attributes cs a <- (getAttributes &&& id -> (cs,a)) where
     Attributes cs a = setAttributes cs a
-       
+
 infixl 2 %
 (%) c as = Attributes as c
+       
+instance HasAttributes (View ms) where
+    type Attribute (View ms) = Feature ms
+    getAttributes v =
+        case v of
+            HTMLView _ _ fs _    -> fs
+            KHTMLView _ _ fs _ _ -> fs
+            SVGView _ _ fs _     -> fs
+            KSVGView _ _ fs _ _  -> fs
+            ManagedView _ _ fs _ -> fs
+            _                    -> []
+    setAttributes fs v =
+        case v of
+            HTMLView e t _ cs    -> HTMLView e t fs cs
+            KHTMLView e t _ cs m -> KHTMLView e t fs cs m
+            SVGView e t _ cs     -> SVGView e t fs cs
+            KSVGView e t _ cs m  -> KSVGView e t fs cs m
+            ManagedView e t _ c  -> ManagedView e t fs c
+            _                    -> v
+    
 
