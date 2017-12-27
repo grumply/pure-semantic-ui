7b456fcb45ae0e9d8c5849f5db678fd8e2f5300a
grumply <sean@grump.ly>
2017-12-26 18:26:26 -0600

    Add HasChildren instance for View. Closes #58.

--- src/Semantic/Extensions/Children.hs
+++ src/Semantic/Extensions/Children.hs
@@ -1,5 +1,7 @@
 module Semantic.Extensions.Children where
 
+import Pure.View
+
 import Semantic.Extensions.Utils
 
 class HasChildren a where
@@ -13,4 +15,25 @@ pattern Children cs a <- (getChildren &&& id -> (cs,a)) where
 
 infixl 1 !
 (!) c cs = Children cs c
+       
+instance HasChildren (View ms) where
+    type Child (View ms) = View ms
+
+    -- Note: keyed nodes are extracted without their index
+    getChildren v =
+        case v of
+            HTMLView _ _ _ cs    -> cs
+            KHTMLView _ _ _ ks _ -> map snd ks
+            SVGView _ _ _ cs     -> cs
+            KSVGView _ _ _ ks _  -> map snd ks
+            _                    -> []
+
+    -- Note: keyed nodes are injected with (zip [0..])
+    setChildren cs v =
+        case v of
+            HTMLView e t fs _    -> HTMLView e t fs cs
+            KHTMLView _ t fs _ m -> list (mkHTML t) fs (zip [0..] cs)
+            SVGView e t fs _     -> SVGView e t fs cs
+            KSVGView _ t fs _ m  -> list (mkSVG t) fs (zip [0..] cs)
+            _                    -> v
 
