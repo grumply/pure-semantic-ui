4b652372101c8e4b9033ec4ab815d93599be1a83
grumply <sean@grump.ly>
2017-12-27 19:27:08 -0600

    Implement StepContent properties. Closes #148.

--- src/Semantic/Elements/Step/StepContent.hs
+++ src/Semantic/Elements/Step/StepContent.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Properties.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data StepContent ms = StepContent_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure StepContent ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (StepContent ms) where
+    type AsProp (StepContent ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a sc = sc { as = a }
+
+instance HasAttributesProp (StepContent ms) where
+    type Attribute (StepContent ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as sc = sc { attributes = as }
+
+instance HasChildrenProp (StepContent ms) where
+    type Child (StepContent ms) = View ms
+    getChildren = children
+    setChildren cs sc = sc { children = cs }
+
+instance HasClassesProp (StepContent ms) where
+    getClasses = classes
+    setClasses cs sc = sc { classes = cs }
