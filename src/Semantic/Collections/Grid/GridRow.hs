6ee994fca4a44f975c3db906c23e16fdf23fb5c1
grumply <sean@grump.ly>
2017-12-28 12:46:39 -0600

    Implement GridRow properties. Closes #174.

--- src/Semantic/Collections/Grid/GridRow.hs
+++ src/Semantic/Collections/Grid/GridRow.hs
@@ -1,9 +1,23 @@
 module Semantic.Collections.Grid.GridRow where
 
 import GHC.Generics as G
-import Pure.View
+import Pure.View hiding (color,textAlign,verticalAlign)
 
 import Semantic.Utils
+
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Centered
+import Semantic.Properties.Color
+import Semantic.Properties.Columns
+import Semantic.Properties.Divided
+import Semantic.Properties.Only
+import Semantic.Properties.Reversed
+import Semantic.Properties.Stretched
+import Semantic.Properties.TextAlign
+import Semantic.Properties.VerticalAlign
 
 data GridRow ms = GridRow_
     { as :: [Feature ms] -> [View ms] -> View ms
@@ -49,3 +63,56 @@ instance Typeable ms => Pure GridRow ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (GridRow ms) where
+    type AsProp (GridRow ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a gr = gr { as = a }
+
+instance HasAttributesProp (GridRow ms) where
+    type Attribute (GridRow ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as gr = gr { attributes = as }
+
+instance HasChildrenProp (GridRow ms) where
+    type Child (GridRow ms) = View ms
+    getChildren = children
+    setChildren cs gr = gr { children = cs }
+
+instance HasClassesProp (GridRow ms) where
+    getClasses = classes
+    setClasses cs gr = gr { classes = cs }
+
+instance HasColorProp (GridRow ms) where
+    getColor = color
+    setColor c gr = gr { color = c }
+
+instance HasColumnsProp (GridRow ms) where
+    getColumns = columns
+    setColumns c gr = gr { columns = c }
+
+instance HasDividedProp (GridRow ms) where
+    getDivided = divided
+    setDivided d gr = gr { divided = d }
+
+instance HasOnlyProp (GridRow ms) where
+    getOnly = only
+    setOnly o gr = gr { only = o }
+
+instance HasReversedProp (GridRow ms) where
+    type ReversedProp (GridRow ms) = [Txt]
+    getReversed = reversed
+    setReversed r gr = gr { reversed = r }
+
+instance HasStretchedProp (GridRow ms) where
+    getStretched = stretched
+    setStretched s gr = gr { stretched = s }
+
+instance HasTextAlignProp (GridRow ms) where
+    getTextAlign = textAlign
+    setTextAlign t gr = gr { textAlign = t }
+
+instance HasVerticalAlignProp (GridRow ms) where
+    getVerticalAlign = verticalAlign
+    setVerticalAlign v gr = gr { verticalAlign = v }
+
