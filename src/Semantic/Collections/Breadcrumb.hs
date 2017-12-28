a8dd86cddd3e059078fd0a0e0e1562cb506d405b
grumply <sean@grump.ly>
2017-12-27 22:30:37 -0600

    Implement Breadcrumb properties. Closes #158.

--- src/Semantic/Collections/Breadcrumb.hs
+++ src/Semantic/Collections/Breadcrumb.hs
@@ -8,6 +8,12 @@ import Semantic.Utils
 import Semantic.Collections.Breadcrumb.BreadcrumbDivider as Export
 import Semantic.Collections.Breadcrumb.BreadcrumbSection as Export
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Size
+
 data Breadcrumb ms = Breadcrumb_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -37,3 +43,26 @@ instance Typeable ms => Pure Breadcrumb ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (Breadcrumb ms) where
+    type AsProp (Breadcrumb ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a bc = bc { as = a }
+
+instance HasAttributesProp (Breadcrumb ms) where
+    type Attribute (Breadcrumb ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as bc = bc { attributes = as }
+
+instance HasChildrenProp (Breadcrumb ms) where
+    type Child (Breadcrumb ms) = View ms
+    getChildren = children
+    setChildren cs bc = bc { children = cs }
+
+instance HasClassesProp (Breadcrumb ms) where
+    getClasses = classes
+    setClasses cs bc = bc { classes = cs }
+
+instance HasSizeProp (Breadcrumb ms) where
+    getSize = size
+    setSize sz bc = bc { size = sz }
