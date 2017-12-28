337ee6b5cd443970d63f437205f09187c54ce317
grumply <sean@grump.ly>
2017-12-27 19:28:56 -0600

    Implement StepDescription properties. Closes #149.

--- src/Semantic/Elements/Step/StepDescription.hs
+++ src/Semantic/Elements/Step/StepDescription.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Properties.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data StepDescription ms = StepDescription_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure StepDescription ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (StepDescription ms) where
+    type AsProp (StepDescription ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a sd = sd { as = a }
+
+instance HasAttributesProp (StepDescription ms) where
+    type Attribute (StepDescription ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as sd = sd { attributes = as }
+
+instance HasChildrenProp (StepDescription ms) where
+    type Child (StepDescription ms) = View ms
+    getChildren = children
+    setChildren cs sd = sd { children = cs }
+
+instance HasClassesProp (StepDescription ms) where
+    getClasses = classes
+    setClasses cs sd = sd { classes = cs }
