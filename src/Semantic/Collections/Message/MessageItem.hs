a7f9bfcd5fea284a1f69720d551c391f49e4a206
grumply <sean@grump.ly>
2017-12-29 03:36:32 -0600

    Implement MessageItem properties. Closes #190.

--- src/Semantic/Collections/Message/MessageItem.hs
+++ src/Semantic/Collections/Message/MessageItem.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data MessageItem ms = MessageItem_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,23 @@ instance Typeable ms => Pure MessageItem ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (MessageItem ms) where
+    type AsProp (MessageItem ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a mi = mi { as = a }
+
+instance HasAttributesProp (MessageItem ms) where
+    type Attribute (MessageItem ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as mi = mi { attributes = as }
+
+instance HasChildrenProp (MessageItem ms) where
+    type Child (MessageItem ms) = View ms
+    getChildren = children
+    setChildren cs mi = mi { children = cs }
+
+instance HasClassesProp (MessageItem ms) where
+    getClasses = classes
+    setClasses cs mi = mi { classes = cs }
+
