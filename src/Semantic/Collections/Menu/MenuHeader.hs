0ebf72c12e0c42cd9932a5bc9a476f547a080c71
grumply <sean@grump.ly>
2017-12-29 00:55:17 -0600

    Implement MenuHeader properties. Closes #178.

--- src/Semantic/Collections/Menu/MenuHeader.hs
+++ src/Semantic/Collections/Menu/MenuHeader.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data MenuHeader ms = MenuHeader_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,23 @@ instance Typeable ms => Pure MenuHeader ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (MenuHeader ms) where
+    type AsProp (MenuHeader ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a mh = mh { as = a }
+
+instance HasAttributesProp (MenuHeader ms) where
+    type Attribute (MenuHeader ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as mh = mh { attributes = as }
+
+instance HasChildrenProp (MenuHeader ms) where
+    type Child (MenuHeader ms) = View ms
+    getChildren = children
+    setChildren cs mh = mh { children = cs }
+
+instance HasClassesProp (MenuHeader ms) where
+    getClasses = classes
+    setClasses cs mh = mh { classes = cs }
+
