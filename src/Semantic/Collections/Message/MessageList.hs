ea65db02c40a798b84ca747ebbdd6280541e4403
grumply <sean@grump.ly>
2017-12-29 03:39:53 -0600

    Implement MessageList properties. Closes #192.

--- src/Semantic/Collections/Message/MessageList.hs
+++ src/Semantic/Collections/Message/MessageList.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data MessageList ms = MessageList_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure MessageList ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (MessageList ms) where
+    type AsProp (MessageList ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a ml = ml { as = a }
+
+instance HasAttributesProp (MessageList ms) where
+    type Attribute (MessageList ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as ml = ml { attributes = as }
+
+instance HasChildrenProp (MessageList ms) where
+    type Child (MessageList ms) = View ms
+    getChildren = children
+    setChildren cs ml = ml { children = cs }
+
+instance HasClassesProp (MessageList ms) where
+    getClasses = classes
+    setClasses cs ml = ml { classes = cs }
