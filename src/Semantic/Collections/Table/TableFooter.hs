7464fc8f6854b70f21783d7d859a896535bfe682
grumply <sean@grump.ly>
2017-12-29 15:50:46 -0600

    Implement TableFooter properties. Closes #200.

--- src/Semantic/Collections/Table/TableFooter.hs
+++ src/Semantic/Collections/Table/TableFooter.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data TableFooter ms = TableFooter_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -25,3 +30,24 @@ instance Typeable ms => Pure TableFooter ms where
             : attributes
             )
             children
+
+instance HasAsProp (TableFooter ms) where
+    type AsProp (TableFooter ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a tf = tf { as = a }
+
+instance HasAttributesProp (TableFooter ms) where
+    type Attribute (TableFooter ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as tf = tf { attributes = as }
+
+instance HasChildrenProp (TableFooter ms) where
+    type Child (TableFooter ms) = View ms
+    getChildren = children
+    setChildren cs tf = tf { children = cs }
+
+instance HasClassesProp (TableFooter ms) where
+    getClasses = classes
+    setClasses cs tf = tf { classes = cs }
+
+    
