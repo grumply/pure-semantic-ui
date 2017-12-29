66e44012b5359a8d7ebc26ec536ba56d0d931724
grumply <sean@grump.ly>
2017-12-29 01:07:37 -0600

    Implement MenuMenu properties. Closes #182.

--- src/Semantic/Collections/Menu/MenuMenu.hs
+++ src/Semantic/Collections/Menu/MenuMenu.hs
@@ -1,9 +1,15 @@
 module Semantic.Collections.Menu.MenuMenu where
 
 import GHC.Generics as G
-import Pure.View
+import Pure.View hiding (position)
 
 import Semantic.Utils
+
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Position
 
 data MenuMenu ms = MenuMenu_
     { as :: [Feature ms] -> [View ms] -> View ms
@@ -33,3 +39,26 @@ instance Typeable ms => Pure MenuMenu ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (MenuMenu ms) where
+    type AsProp (MenuMenu ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a mm = mm { as = a }
+
+instance HasAttributesProp (MenuMenu ms) where
+    type Attribute (MenuMenu ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as mm = mm { attributes = as }
+
+instance HasChildrenProp (MenuMenu ms) where
+    type Child (MenuMenu ms) = View ms
+    getChildren = children
+    setChildren cs mm = mm { children = cs }
+
+instance HasClassesProp (MenuMenu ms) where
+    getClasses = classes
+    setClasses cs mm = mm { classes = cs }
+
+instance HasPositionProp (MenuMenu ms) where
+    getPosition = position
+    setPosition p mm = mm { position = p }
