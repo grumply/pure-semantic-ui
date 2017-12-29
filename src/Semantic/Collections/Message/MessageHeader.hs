7f300eff35267c39d7329091a912531a7ab61dd9
grumply <sean@grump.ly>
2017-12-29 03:31:13 -0600

    Implement MessageHeader properties. Closes #188.

--- src/Semantic/Collections/Message/MessageHeader.hs
+++ src/Semantic/Collections/Message/MessageHeader.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data MessageHeader ms = MessageHeader_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,24 @@ instance Typeable ms => Pure MessageHeader ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (MessageHeader ms) where
+    type AsProp (MessageHeader ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a mc = mc { as = a }
+
+instance HasAttributesProp (MessageHeader ms) where
+    type Attribute (MessageHeader ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as mc = mc { attributes = as }
+
+instance HasChildrenProp (MessageHeader ms) where
+    type Child (MessageHeader ms) = View ms
+    getChildren = children
+    setChildren cs mc = mc { children = cs }
+
+instance HasClassesProp (MessageHeader ms) where
+    getClasses = classes
+    setClasses cs mc = mc { classes = cs }
+
+    
