c6798d86de6f7684c2e9f2087c052588fa2e342c
S. M. Hickman <sean@grump.ly>
2017-12-30 01:14:04 -0600

    Implement FeedMeta properties. Closes #255.

--- src/Semantic/Views/Feed/FeedMeta.hs
+++ src/Semantic/Views/Feed/FeedMeta.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data FeedMeta ms = FeedMeta_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure FeedMeta ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (FeedMeta ms) where
+    type AsProp (FeedMeta ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a fm = fm { as = a }
+
+instance HasAttributesProp (FeedMeta ms) where
+    type Attribute (FeedMeta ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as fm = fm { attributes = as }
+
+instance HasChildrenProp (FeedMeta ms) where
+    type Child (FeedMeta ms) = View ms
+    getChildren = children
+    setChildren cs fm = fm { children = cs }
+
+instance HasClassesProp (FeedMeta ms) where
+    getClasses = classes
+    setClasses cs fm = fm { classes = cs }
