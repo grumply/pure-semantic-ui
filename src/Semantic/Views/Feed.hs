6634c8eb67362aaecad6b6897272c151e3e322c5
S. M. Hickman <sean@grump.ly>
2017-12-30 01:09:10 -0600

    Implement Feed properties. Closes #241.

--- src/Semantic/Views/Feed.hs
+++ src/Semantic/Views/Feed.hs
@@ -5,6 +5,12 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Size
+
 data Feed ms = Feed_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -34,3 +40,26 @@ instance Typeable ms => Pure Feed ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (Feed ms) where
+    type AsProp (Feed ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a f = f { as = a }
+
+instance HasAttributesProp (Feed ms) where
+    type Attribute (Feed ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as f = f { attributes = as }
+
+instance HasChildrenProp (Feed ms) where
+    type Child (Feed ms) = View ms
+    getChildren = children
+    setChildren cs f = f { children = cs }
+
+instance HasClassesProp (Feed ms) where
+    getClasses = classes
+    setClasses cs f = f { classes = cs }
+
+instance HasSizeProp (Feed ms) where
+    getSize = size
+    setSize s f = f { size = s }
