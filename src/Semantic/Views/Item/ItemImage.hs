3ba874254122084f30f5bfe2ed64e892d2e01d37
S. M. Hickman <sean@grump.ly>
2017-12-30 01:50:24 -0600

    Implement ItemImage properties. Closes #273.

--- src/Semantic/Views/Item/ItemImage.hs
+++ src/Semantic/Views/Item/ItemImage.hs
@@ -5,6 +5,26 @@ import Pure.View as View hiding (disabled,hidden,inline,verticalAlign)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Avatar
+import Semantic.Properties.Bordered
+import Semantic.Properties.Centered
+import Semantic.Properties.Children
+import Semantic.Properties.Circular
+import Semantic.Properties.Classes
+import Semantic.Properties.Disabled
+import Semantic.Properties.Floated
+import Semantic.Properties.Fluid
+import Semantic.Properties.Hidden
+import Semantic.Properties.Inline
+import Semantic.Properties.Rounded
+import Semantic.Properties.Size
+import Semantic.Properties.Spaced
+import Semantic.Properties.UI
+import Semantic.Properties.VerticalAlign
+import Semantic.Properties.Wrapped
+
 data ItemImage ms = ItemImage_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -59,3 +79,79 @@ instance Typeable ms => Pure ItemImage ms where
                 : attributes
                 )
                 children
+
+instance HasAvatarProp (ItemImage ms) where
+    getAvatar = avatar
+    setAvatar a i = i { avatar = a }
+
+instance HasAsProp (ItemImage ms) where
+    type AsProp (ItemImage ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs f i = i { as = f }
+
+instance HasAttributesProp (ItemImage ms) where
+    type Attribute (ItemImage ms) = Feature ms
+    getAttributes = attributes 
+    setAttributes cs i = i { attributes = cs }
+
+instance HasBorderedProp (ItemImage ms) where
+    getBordered = bordered
+    setBordered b i = i { bordered = b }
+
+instance HasCenteredProp (ItemImage ms) where
+    getCentered = centered
+    setCentered c i = i { centered = c }
+
+instance HasChildrenProp (ItemImage ms) where
+    type Child (ItemImage ms) = View ms
+    getChildren = children
+    setChildren cs i = i { children = cs }
+
+instance HasCircularProp (ItemImage ms) where
+    getCircular = circular
+    setCircular c i = i { circular = c }
+
+instance HasClassesProp (ItemImage ms) where
+    getClasses = classes
+    setClasses cs i = i { classes = cs }
+
+instance HasDisabledProp (ItemImage ms) where
+    getDisabled = disabled
+    setDisabled d i = i { disabled = d }
+
+instance HasFloatedProp (ItemImage ms) where
+    getFloated = floated
+    setFloated f i = i { floated = f }
+
+instance HasFluidProp (ItemImage ms) where
+    getFluid = fluid
+    setFluid f i = i { fluid = f }
+
+instance HasInlineProp (ItemImage ms) where
+    type InlineProp (ItemImage ms) = Bool
+    getInline = inline
+    setInline inl i = i { inline = inl }
+
+instance HasHiddenProp (ItemImage ms) where
+    getHidden = hidden
+    setHidden h i = i { hidden = h }
+
+instance HasRoundedProp (ItemImage ms) where
+    getRounded = rounded
+    setRounded r i = i { rounded = r }
+
+instance HasSizeProp (ItemImage ms) where
+    getSize = size
+    setSize s i = i { size = s }
+
+instance HasSpacedProp (ItemImage ms) where
+    getSpaced = spaced
+    setSpaced s i = i { spaced = s }
+
+instance HasVerticalAlignProp (ItemImage ms) where
+    getVerticalAlign = verticalAlign
+    setVerticalAlign va i = i { verticalAlign = va }
+
+instance HasWrappedProp (ItemImage ms) where
+    getWrapped = wrapped
+    setWrapped w i = i { wrapped = w }
