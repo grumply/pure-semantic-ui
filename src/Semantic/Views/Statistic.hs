919d03cfda21562b6cc8858e3524e61ab27a059a
S. M. Hickman <sean@grump.ly>
2017-12-30 02:22:13 -0600

    Implement Statistic properties. Closes #277.

--- src/Semantic/Views/Statistic.hs
+++ src/Semantic/Views/Statistic.hs
@@ -5,6 +5,16 @@ import Pure.View hiding (color,horizontal)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Color
+import Semantic.Properties.Floated
+import Semantic.Properties.Horizontal
+import Semantic.Properties.Inverted
+import Semantic.Properties.Size
+
 data Statistic ms = Statistic_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -42,3 +52,42 @@ instance Typeable ms => Pure Statistic ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (Statistic ms) where
+    type AsProp (Statistic ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a s = s { as = a }
+
+instance HasAttributesProp (Statistic ms) where
+    type Attribute (Statistic ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as s = s { attributes = as }
+
+instance HasChildrenProp (Statistic ms) where
+    type Child (Statistic ms) = View ms
+    getChildren = children
+    setChildren cs s = s { children = cs }
+
+instance HasClassesProp (Statistic ms) where
+    getClasses = classes
+    setClasses cs s = s { classes = cs }
+
+instance HasColorProp (Statistic ms) where
+    getColor = color
+    setColor c s = s { color = c }
+
+instance HasFloatedProp (Statistic ms) where
+    getFloated = floated
+    setFloated f s = s { floated = f }
+
+instance HasHorizontalProp (Statistic ms) where
+    getHorizontal = horizontal
+    setHorizontal h s = s { horizontal = h }
+
+instance HasInvertedProp (Statistic ms) where
+    getInverted = inverted
+    setInverted i s = s { inverted = i }
+
+instance HasSizeProp (Statistic ms) where
+    getSize = size
+    setSize sz s = s { size = sz }
