c22cbfd53ad1952ee9313139ec8618c97f67349f
S. M. Hickman <sean@grump.ly>
2017-12-30 01:48:58 -0600

    Implement ItemGroup properties. Closes #269.

--- src/Semantic/Views/Item/ItemGroup.hs
+++ src/Semantic/Views/Item/ItemGroup.hs
@@ -4,6 +4,16 @@ import GHC.Generics as G
 import Pure.View
 
 import Semantic.Utils
+
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Divided
+import Semantic.Properties.Link
+import Semantic.Properties.Relaxed
+import Semantic.Properties.Unstackable
+
 data ItemGroup ms = ItemGroup_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -39,3 +49,38 @@ instance Typeable ms => Pure ItemGroup ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (ItemGroup ms) where
+    type AsProp (ItemGroup ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a ig = ig { as = a }
+
+instance HasAttributesProp (ItemGroup ms) where
+    type Attribute (ItemGroup ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as ig = ig { attributes = as }
+
+instance HasChildrenProp (ItemGroup ms) where
+    type Child (ItemGroup ms) = View ms
+    getChildren = children
+    setChildren cs ig = ig { children = cs }
+
+instance HasClassesProp (ItemGroup ms) where
+    getClasses = classes
+    setClasses cs ig = ig { classes = cs }
+
+instance HasDividedProp (ItemGroup ms) where
+    getDivided = divided
+    setDivided d ig = ig { divided = d }
+
+instance HasLinkProp (ItemGroup ms) where
+    getLink = link
+    setLink l ig = ig { link = l }
+
+instance HasRelaxedProp (ItemGroup ms) where
+    getRelaxed = relaxed
+    setRelaxed r ig = ig { relaxed = r }
+
+instance HasUnstackableProp (ItemGroup ms) where
+    getUnstackable = unstackable
+    setUnstackable u ig = ig { unstackable = u }
