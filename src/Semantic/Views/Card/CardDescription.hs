8bcd3e3ca57b2a52b1721f554e8ef4a8e5d6f27b
S. M. Hickman <sean@grump.ly>
2017-12-29 22:53:32 -0600

    Implement CardDescription properties. Closes #215.

--- src/Semantic/Views/Card/CardDescription.hs
+++ src/Semantic/Views/Card/CardDescription.hs
@@ -1,9 +1,44 @@
+module Semantic.Views.Card.CardDescription where
+
+import GHC.Generics as G
+import Pure.View hiding (textAlign)
+
+import Semantic.Utils
+
 import Semantic.Properties.As
 import Semantic.Properties.Attributes
 import Semantic.Properties.Children
 import Semantic.Properties.Classes
 import Semantic.Properties.TextAlign
 
+data CardDescription ms = CardDescription_
+    { as :: [Feature ms] -> [View ms] -> View ms
+    , attributes :: [Feature ms]
+    , children :: [View ms]
+    , classes :: [Txt]
+    , textAlign :: Txt
+    } deriving (Generic)
+
+instance Default (CardDescription ms) where
+    def = (G.to gdef) { as = Div }
+
+pattern CardDescription :: Typeable ms => CardDescription ms -> View ms
+pattern CardDescription cd = View cd
+
+instance Typeable ms => Pure CardDescription ms where
+    render CardDescription_ {..} =
+        let
+            cs =
+                ( textAlign
+                : "description"
+                : classes
+                )
+        in
+            as
+                ( ClassList cs
+                : attributes
+                )
+                children
 
 instance HasAsProp (CardDescription ms) where
     type AsProp (CardDescription ms) = [Feature ms] -> [View ms] -> View ms
