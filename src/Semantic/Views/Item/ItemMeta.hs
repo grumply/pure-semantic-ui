60c5497f878759a6ca40789b8145eb8090892824
S. M. Hickman <sean@grump.ly>
2017-12-30 01:51:02 -0600

    Implement ItemMeta properties. Closes #275.

--- src/Semantic/Views/Item/ItemMeta.hs
+++ src/Semantic/Views/Item/ItemMeta.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data ItemMeta ms = ItemMeta_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure ItemMeta ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (ItemMeta ms) where
+    type AsProp (ItemMeta ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a im = im { as = a }
+
+instance HasAttributesProp (ItemMeta ms) where
+    type Attribute (ItemMeta ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as im = im { attributes = as }
+
+instance HasChildrenProp (ItemMeta ms) where
+    type Child (ItemMeta ms) = View ms
+    getChildren = children
+    setChildren cs im = im { children = cs }
+
+instance HasClassesProp (ItemMeta ms) where
+    getClasses = classes
+    setClasses cs im = im { classes = cs }
