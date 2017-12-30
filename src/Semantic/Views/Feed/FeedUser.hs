5b704f6ca19a558fdf3a04bec12936bb51a7616f
S. M. Hickman <sean@grump.ly>
2017-12-30 01:15:22 -0600

    Implement FeedUser properties. Closes #259.

--- src/Semantic/Views/Feed/FeedUser.hs
+++ src/Semantic/Views/Feed/FeedUser.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data FeedUser ms = FeedUser_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure FeedUser ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (FeedUser ms) where
+    type AsProp (FeedUser ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a fu = fu { as = a }
+
+instance HasAttributesProp (FeedUser ms) where
+    type Attribute (FeedUser ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as fu = fu { attributes = as }
+
+instance HasChildrenProp (FeedUser ms) where
+    type Child (FeedUser ms) = View ms
+    getChildren = children
+    setChildren cs fu = fu { children = cs }
+
+instance HasClassesProp (FeedUser ms) where
+    getClasses = classes
+    setClasses cs fu = fu { classes = cs }
