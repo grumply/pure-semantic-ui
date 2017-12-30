3edb8d0fed9187642156eaed445b1a55fd1c2a16
S. M. Hickman <sean@grump.ly>
2017-12-30 01:48:22 -0600

    Implement ItemExtra properties. Closes #267.

--- src/Semantic/Views/Item/ItemExtra.hs
+++ src/Semantic/Views/Item/ItemExtra.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data ItemExtra ms = ItemExtra_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure ItemExtra ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (ItemExtra ms) where
+    type AsProp (ItemExtra ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a ie = ie { as = a }
+
+instance HasAttributesProp (ItemExtra ms) where
+    type Attribute (ItemExtra ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as ie = ie { attributes = as }
+
+instance HasChildrenProp (ItemExtra ms) where
+    type Child (ItemExtra ms) = View ms
+    getChildren = children
+    setChildren cs ie = ie { children = cs }
+
+instance HasClassesProp (ItemExtra ms) where
+    getClasses = classes
+    setClasses cs ie = ie { classes = cs }
