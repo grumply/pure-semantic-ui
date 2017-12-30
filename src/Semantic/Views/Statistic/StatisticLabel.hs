a887aeaf0d982f9233deaa56541a0a3de7f90dc6
S. M. Hickman <sean@grump.ly>
2017-12-30 02:23:43 -0600

    Implement StatisticLabel properties. Closes #281.

--- src/Semantic/Views/Statistic/StatisticLabel.hs
+++ src/Semantic/Views/Statistic/StatisticLabel.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data StatisticLabel ms = StatisticLabel_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure StatisticLabel ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (StatisticLabel ms) where
+    type AsProp (StatisticLabel ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a sl = sl { as = a }
+
+instance HasAttributesProp (StatisticLabel ms) where
+    type Attribute (StatisticLabel ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as sl = sl { attributes = as }
+
+instance HasChildrenProp (StatisticLabel ms) where
+    type Child (StatisticLabel ms) = View ms
+    getChildren = children
+    setChildren cs sl = sl { children = cs }
+
+instance HasClassesProp (StatisticLabel ms) where
+    getClasses = classes
+    setClasses cs sl = sl { classes = cs }
