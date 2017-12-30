c4107e202cab232db423d58cf02f8dac5842788d
S. M. Hickman <sean@grump.ly>
2017-12-30 02:22:59 -0600

    Implement StatisticGroup properties. Closes #279.

--- src/Semantic/Views/Statistic/StatisticGroup.hs
+++ src/Semantic/Views/Statistic/StatisticGroup.hs
@@ -5,6 +5,16 @@ import Pure.View hiding (color,horizontal,widths)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Color
+import Semantic.Properties.Horizontal
+import Semantic.Properties.Inverted
+import Semantic.Properties.Size
+import Semantic.Properties.Widths
+
 data StatisticGroup ms = StatisticGroup_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -42,3 +52,42 @@ instance Typeable ms => Pure StatisticGroup ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (StatisticGroup ms) where
+    type AsProp (StatisticGroup ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a sg = sg { as = a }
+
+instance HasAttributesProp (StatisticGroup ms) where
+    type Attribute (StatisticGroup ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as sg = sg { attributes = as }
+
+instance HasChildrenProp (StatisticGroup ms) where
+    type Child (StatisticGroup ms) = View ms
+    getChildren = children
+    setChildren cs sg = sg { children = cs }
+
+instance HasClassesProp (StatisticGroup ms) where
+    getClasses = classes
+    setClasses cs sg = sg { classes = cs }
+
+instance HasColorProp (StatisticGroup ms) where
+    getColor = color
+    setColor c s = s { color = c }
+
+instance HasHorizontalProp (StatisticGroup ms) where
+    getHorizontal = horizontal
+    setHorizontal h s = s { horizontal = h }
+
+instance HasInvertedProp (StatisticGroup ms) where
+    getInverted = inverted
+    setInverted i s = s { inverted = i }
+
+instance HasSizeProp (StatisticGroup ms) where
+    getSize = size
+    setSize sz s = s { size = sz }
+
+instance HasWidthsProp (StatisticGroup ms) where
+    getWidths = widths
+    setWidths w s = s { widths = w }
