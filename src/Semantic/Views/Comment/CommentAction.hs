cebc5b398332a80f4c351519165f483c7d93e30f
S. M. Hickman <sean@grump.ly>
2017-12-30 00:25:46 -0600

    Implement CommentAction properties. Closes #225.

--- src/Semantic/Views/Comment/CommentAction.hs
+++ src/Semantic/Views/Comment/CommentAction.hs
@@ -5,6 +5,12 @@ import Pure.View hiding (active)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Active
+
 data CommentAction ms = CommentAction_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -32,3 +38,26 @@ instance Typeable ms => Pure CommentAction ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (CommentAction ms) where
+    type AsProp (CommentAction ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a ca = ca { as = a }
+
+instance HasAttributesProp (CommentAction ms) where
+    type Attribute (CommentAction ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as ca = ca { attributes = as }
+
+instance HasChildrenProp (CommentAction ms) where
+    type Child (CommentAction ms) = View ms
+    getChildren = children
+    setChildren cs ca = ca { children = cs }
+
+instance HasClassesProp (CommentAction ms) where
+    getClasses = classes
+    setClasses cs ca = ca { classes = cs }
+
+instance HasActiveProp (CommentAction ms) where
+    getActive = active
+    setActive a ca = ca { active = a }
