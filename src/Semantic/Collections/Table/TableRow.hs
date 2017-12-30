3ab5c941583a26294e2a3e2639883883cbf6b9ca
S. M. Hickman <sean@grump.ly>
2017-12-29 20:42:31 -0600

    Implement TableRow properties. Closes #206.

--- src/Semantic/Collections/Table/TableRow.hs
+++ src/Semantic/Collections/Table/TableRow.hs
@@ -3,6 +3,22 @@ module Semantic.Collections.Table.TableRow where
 import GHC.Generics as G
 import Pure.View hiding (active,disabled,textAlign,verticalAlign)
 
+import Semantic.Utils
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Active
+import Semantic.Properties.Disabled
+import Semantic.Properties.Error
+import Semantic.Properties.Negative
+import Semantic.Properties.Positive
+import Semantic.Properties.TextAlign
+import Semantic.Properties.VerticalAlign
+import Semantic.Properties.Warning
+
+import Prelude hiding (error)
+
 data TableRow ms = TableRow_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -44,3 +60,54 @@ instance Typeable ms => Pure TableRow ms where
                 : attributes
                 )
                 children
+        
+instance HasAsProp (TableRow ms) where
+    type AsProp (TableRow ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a tr = tr { as = a }
+
+instance HasAttributesProp (TableRow ms) where
+    type Attribute (TableRow ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as tr = tr { attributes = as }
+
+instance HasChildrenProp (TableRow ms) where
+    type Child (TableRow ms) = View ms
+    getChildren = children
+    setChildren cs tr = tr { children = cs }
+
+instance HasClassesProp (TableRow ms) where
+    getClasses = classes
+    setClasses cs tr = tr { classes = cs }
+
+instance HasActiveProp (TableRow ms) where
+    getActive = active
+    setActive a tr = tr { active = a }
+
+instance HasDisabledProp (TableRow ms) where
+    getDisabled = disabled
+    setDisabled d tr = tr { disabled = d }
+
+instance HasErrorProp (TableRow ms) where
+    getError = error
+    setError e tr = tr { error = e }
+
+instance HasNegativeProp (TableRow ms) where
+    getNegative = negative
+    setNegative n tr = tr { negative = n }
+
+instance HasPositiveProp (TableRow ms) where
+    getPositive = positive
+    setPositive p tr = tr { positive = p }
+
+instance HasTextAlignProp (TableRow ms) where
+    getTextAlign = textAlign
+    setTextAlign ta tr = tr { textAlign = ta }
+
+instance HasVerticalAlignProp (TableRow ms) where
+    getVerticalAlign = verticalAlign
+    setVerticalAlign va tr = tr { verticalAlign = va }
+
+instance HasWarningProp (TableRow ms) where
+    getWarning = warning
+    setWarning w tr = tr { warning = w }
