dd5e6e9e03fa839ebe032e58783f038f14182a03
S. M. Hickman <sean@grump.ly>
2017-12-30 02:24:33 -0600

    Implement StatisticValue properties. Closes #283.

--- src/Semantic/Views/Statistic/StatisticValue.hs
+++ src/Semantic/Views/Statistic/StatisticValue.hs
@@ -5,6 +5,12 @@ import Pure.View hiding (text)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.IsText
+
 data StatisticValue ms = StatisticValue_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -33,3 +39,26 @@ instance Typeable ms => Pure StatisticValue ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (StatisticValue ms) where
+    type AsProp (StatisticValue ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a sv = sv { as = a }
+
+instance HasAttributesProp (StatisticValue ms) where
+    type Attribute (StatisticValue ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as sv = sv { attributes = as }
+
+instance HasChildrenProp (StatisticValue ms) where
+    type Child (StatisticValue ms) = View ms
+    getChildren = children
+    setChildren cs sv = sv { children = cs }
+
+instance HasClassesProp (StatisticValue ms) where
+    getClasses = classes
+    setClasses cs sv = sv { classes = cs }
+
+instance HasIsTextProp (StatisticValue ms) where
+    getIsText = text
+    setIsText it sv = sv { text = it }
