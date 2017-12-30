5fb798291a62d4d97667ebf3b0f564ce4317dee6
S. M. Hickman <sean@grump.ly>
2017-12-30 01:46:19 -0600

    Implement Item properties. Closes #261.

--- src/Semantic/Views/Item.hs
+++ src/Semantic/Views/Item.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data Item ms = Item_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure Item ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (Item ms) where
+    type AsProp (Item ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a i = i { as = a }
+
+instance HasAttributesProp (Item ms) where
+    type Attribute (Item ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as i = i { attributes = as }
+
+instance HasChildrenProp (Item ms) where
+    type Child (Item ms) = View ms
+    getChildren = children
+    setChildren cs i = i { children = cs }
+
+instance HasClassesProp (Item ms) where
+    getClasses = classes
+    setClasses cs i = i { classes = cs }
