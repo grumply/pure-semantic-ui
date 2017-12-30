69376cb2c4dd2acb86850f59409cc4ea92ec2ab1
S. M. Hickman <sean@grump.ly>
2017-12-30 00:30:48 -0600

    Implement CommentContent properties. Closes #233.

--- src/Semantic/Views/Comment/CommentContent.hs
+++ src/Semantic/Views/Comment/CommentContent.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data CommentContent ms = CommentContent_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure CommentContent ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (CommentContent ms) where
+    type AsProp (CommentContent ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a cc = cc { as = a }
+
+instance HasAttributesProp (CommentContent ms) where
+    type Attribute (CommentContent ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as cc = cc { attributes = as }
+
+instance HasChildrenProp (CommentContent ms) where
+    type Child (CommentContent ms) = View ms
+    getChildren = children
+    setChildren cs cc = cc { children = cs }
+
+instance HasClassesProp (CommentContent ms) where
+    getClasses = classes
+    setClasses cs cc = cc { classes = cs }
