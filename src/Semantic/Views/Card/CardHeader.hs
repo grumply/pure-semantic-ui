8b6ce8caf4e2af71deccdfae4db10a28ad2c2b18
S. M. Hickman <sean@grump.ly>
2017-12-29 23:27:17 -0600

    Implement CardHeader properties. Closes #219.

--- src/Semantic/Views/Card/CardHeader.hs
+++ src/Semantic/Views/Card/CardHeader.hs
@@ -5,6 +5,12 @@ import Pure.View hiding (textAlign)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.TextAlign
+
 data CardHeader ms = CardHeader_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -33,3 +39,26 @@ instance Typeable ms => Pure CardHeader ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (CardHeader ms) where
+    type AsProp (CardHeader ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a ch = ch { as = a }
+
+instance HasAttributesProp (CardHeader ms) where
+    type Attribute (CardHeader ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as ch = ch { attributes = as }
+
+instance HasChildrenProp (CardHeader ms) where
+    type Child (CardHeader ms) = View ms
+    getChildren = children
+    setChildren cs ch = ch { children = cs }
+
+instance HasClassesProp (CardHeader ms) where
+    getClasses = classes
+    setClasses cs ch = ch { classes = cs }
+
+instance HasTextAlignProp (CardHeader ms) where
+    getTextAlign = textAlign
+    setTextAlign ta ch = ch { textAlign = ta }
