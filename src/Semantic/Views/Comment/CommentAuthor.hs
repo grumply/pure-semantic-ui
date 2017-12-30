2669d12600136175f1a56f089724b27c060b1018
S. M. Hickman <sean@grump.ly>
2017-12-30 00:27:17 -0600

    Implement CommentAuthor properties. Closes #229.

--- src/Semantic/Views/Comment/CommentAuthor.hs
+++ src/Semantic/Views/Comment/CommentAuthor.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data CommentAuthor ms = CommentAuthor_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure CommentAuthor ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (CommentAuthor ms) where
+    type AsProp (CommentAuthor ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a ca = ca { as = a }
+
+instance HasAttributesProp (CommentAuthor ms) where
+    type Attribute (CommentAuthor ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as ca = ca { attributes = as }
+
+instance HasChildrenProp (CommentAuthor ms) where
+    type Child (CommentAuthor ms) = View ms
+    getChildren = children
+    setChildren cs ca = ca { children = cs }
+
+instance HasClassesProp (CommentAuthor ms) where
+    getClasses = classes
+    setClasses cs ca = ca { classes = cs }
