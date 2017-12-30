47366a7430757e80367cc715622a3afaa568512a
S. M. Hickman <sean@grump.ly>
2017-12-29 20:37:47 -0600

    Implement TableHeaderCell properties. Closes #204.

--- src/Semantic/Collections/Table/TableHeaderCell.hs
+++ src/Semantic/Collections/Table/TableHeaderCell.hs
@@ -1,7 +1,7 @@
 module Semantic.Collections.Table.TableHeaderCell where
 
 import GHC.Generics as G
-import Pure.View
+import Pure.View hiding (active,disabled,textAlign,verticalAlign,width)
 
 import Semantic.Utils
 
@@ -9,9 +9,21 @@ import Semantic.Properties.As
 import Semantic.Properties.Attributes
 import Semantic.Properties.Children
 import Semantic.Properties.Classes
+import Semantic.Properties.Active
+import Semantic.Properties.Collapsing
+import Semantic.Properties.Disabled
+import Semantic.Properties.Error
+import Semantic.Properties.Negative
+import Semantic.Properties.Positive
+import Semantic.Properties.Selectable
+import Semantic.Properties.SingleLine
 import Semantic.Properties.Sorted
+import Semantic.Properties.TextAlign
+import Semantic.Properties.VerticalAlign
+import Semantic.Properties.Warning
+import Semantic.Properties.Width
 
-import Semantic.Properties.TableCell
+import Prelude hiding (error)
 
 data TableHeaderCell ms = TableHeaderCell_
     { as :: [Feature ms] -> [View ms] -> View ms
@@ -84,4 +96,55 @@ instance HasClassesProp (TableHeaderCell ms) where
     getClasses = classes
     setClasses cs thc = thc { classes = cs }
 
-    
+instance HasActiveProp (TableHeaderCell ms) where
+    getActive = active
+    setActive a thc = thc { active = a }
+
+instance HasCollapsingProp (TableHeaderCell ms) where
+    getCollapsing = collapsing
+    setCollapsing c thc = thc { collapsing = c }
+
+instance HasDisabledProp (TableHeaderCell ms) where
+    getDisabled = disabled
+    setDisabled d thc = thc { disabled = d }
+
+instance HasErrorProp (TableHeaderCell ms) where
+    getError = error
+    setError e thc = thc { error = e }
+
+instance HasNegativeProp (TableHeaderCell ms) where
+    getNegative = negative
+    setNegative n thc = thc { negative = n }
+
+instance HasPositiveProp (TableHeaderCell ms) where
+    getPositive = positive
+    setPositive p thc = thc { positive = p }
+
+instance HasSelectableProp (TableHeaderCell ms) where
+    getSelectable = selectable
+    setSelectable s thc = thc { selectable = s }
+
+instance HasSingleLineProp (TableHeaderCell ms) where
+    getSingleLine = singleLine
+    setSingleLine sl thc = thc { singleLine = sl }
+
+instance HasSortedProp (TableHeaderCell ms) where
+    getSorted = sorted
+    setSorted s thc = thc { sorted = s }
+
+instance HasTextAlignProp (TableHeaderCell ms) where
+    getTextAlign = textAlign
+    setTextAlign ta thc = thc { textAlign = ta }
+
+instance HasVerticalAlignProp (TableHeaderCell ms) where
+    getVerticalAlign = verticalAlign
+    setVerticalAlign va thc = thc { verticalAlign = va }
+
+instance HasWarningProp (TableHeaderCell ms) where
+    getWarning = warning
+    setWarning w thc = thc { warning = w }
+
+instance HasWidthProp (TableHeaderCell ms) where
+    getWidth = width
+    setWidth w thc = thc { width = w }
+
