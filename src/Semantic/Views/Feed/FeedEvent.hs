972c0f1290df11af1422fb04f8668d343afbc4df
S. M. Hickman <sean@grump.ly>
2017-12-30 01:11:18 -0600

    Implement FeedEvent properties. Closes #247.

--- src/Semantic/Views/Feed/FeedEvent.hs
+++ src/Semantic/Views/Feed/FeedEvent.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data FeedEvent ms = FeedEvent_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure FeedEvent ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (FeedEvent ms) where
+    type AsProp (FeedEvent ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a fe = fe { as = a }
+
+instance HasAttributesProp (FeedEvent ms) where
+    type Attribute (FeedEvent ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as fe = fe { attributes = as }
+
+instance HasChildrenProp (FeedEvent ms) where
+    type Child (FeedEvent ms) = View ms
+    getChildren = children
+    setChildren cs fe = fe { children = cs }
+
+instance HasClassesProp (FeedEvent ms) where
+    getClasses = classes
+    setClasses cs fe = fe { classes = cs }
