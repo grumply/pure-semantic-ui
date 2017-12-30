9517202c19e80d1e95b2c1c9049fdfc04426c537
S. M. Hickman <sean@grump.ly>
2017-12-29 23:28:00 -0600

    Implement CardMeta properties. Closes #221.

--- src/Semantic/Views/Card/CardMeta.hs
+++ src/Semantic/Views/Card/CardMeta.hs
@@ -5,6 +5,12 @@ import Pure.View hiding (textAlign)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.TextAlign
+
 data CardMeta ms = CardMeta_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -33,3 +39,26 @@ instance Typeable ms => Pure CardMeta ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (CardMeta ms) where
+    type AsProp (CardMeta ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a cm = cm { as = a }
+
+instance HasAttributesProp (CardMeta ms) where
+    type Attribute (CardMeta ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as cm = cm { attributes = as }
+
+instance HasChildrenProp (CardMeta ms) where
+    type Child (CardMeta ms) = View ms
+    getChildren = children
+    setChildren cs cm = cm { children = cs }
+
+instance HasClassesProp (CardMeta ms) where
+    getClasses = classes
+    setClasses cs cm = cm { classes = cs }
+
+instance HasTextAlignProp (CardMeta ms) where
+    getTextAlign = textAlign
+    setTextAlign ta cm = cm { textAlign = ta }
