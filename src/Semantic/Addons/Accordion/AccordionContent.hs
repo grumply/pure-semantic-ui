a9ee5073f3d02a2d5f7b71dad6f1fd9547a10760
grumply <sean@grump.ly>
2017-12-31 02:53:13 -0600

    Implement AccordionContent properties. Closes #293.

--- src/Semantic/Addons/Accordion/AccordionContent.hs
+++ src/Semantic/Addons/Accordion/AccordionContent.hs
@@ -5,6 +5,12 @@ import Pure.View hiding (active)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Active
+
 data AccordionContent ms = AccordionContent_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -33,3 +39,26 @@ instance Typeable ms => Pure AccordionContent ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (AccordionContent ms) where
+    type AsProp (AccordionContent ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a ac = ac { as = a }
+
+instance HasAttributesProp (AccordionContent ms) where
+    type Attribute (AccordionContent ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as ac = ac { attributes = as }
+
+instance HasChildrenProp (AccordionContent ms) where
+    type Child (AccordionContent ms) = View ms
+    getChildren = children
+    setChildren cs ac = ac { children = cs }
+
+instance HasClassesProp (AccordionContent ms) where
+    getClasses = classes
+    setClasses cs ac = ac { classes = cs }
+
+instance HasActiveProp (AccordionContent ms) where
+    getActive = active
+    setActive a ac = ac { active = a }
