77e0218fbeb8c42c5d2e891a4e00e8afea356934
grumply <sean@grump.ly>
2017-12-31 02:53:51 -0600

    Implement AccordionTitle properties. Closes #295.

--- src/Semantic/Addons/Accordion/AccordionTitle.hs
+++ src/Semantic/Addons/Accordion/AccordionTitle.hs
@@ -5,6 +5,14 @@ import Pure.View hiding (active,onClick)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Active
+import Semantic.Properties.Index
+import Semantic.Properties.OnClick
+
 data AccordionTitle ms = AccordionTitle_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -36,3 +44,35 @@ instance Typeable ms => Pure AccordionTitle ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (AccordionTitle ms) where
+    type AsProp (AccordionTitle ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a at = at { as = a }
+
+instance HasAttributesProp (AccordionTitle ms) where
+    type Attribute (AccordionTitle ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as at = at { attributes = as }
+
+instance HasChildrenProp (AccordionTitle ms) where
+    type Child (AccordionTitle ms) = View ms
+    getChildren = children
+    setChildren cs at = at { children = cs }
+
+instance HasClassesProp (AccordionTitle ms) where
+    getClasses = classes
+    setClasses cs at = at { classes = cs }
+
+instance HasActiveProp (AccordionTitle ms) where
+    getActive = active
+    setActive a at = at { active = a }
+
+instance HasIndexProp (AccordionTitle ms) where
+    getIndex = index
+    setIndex i at = at { index = i }
+
+instance HasOnClickProp (AccordionTitle ms) where
+    type OnClickProp (AccordionTitle ms) = Ef ms IO ()
+    getOnClick = onClick
+    setOnClick oc at = at { onClick = oc }
