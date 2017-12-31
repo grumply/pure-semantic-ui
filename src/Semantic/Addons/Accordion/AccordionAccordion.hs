29496c3180522c54b4838d80aa19b5ad98568e11
grumply <sean@grump.ly>
2017-12-31 02:52:22 -0600

    Implement AccordionAccordion properties. Closes #291.

--- src/Semantic/Addons/Accordion/AccordionAccordion.hs
+++ src/Semantic/Addons/Accordion/AccordionAccordion.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data AccordionAccordion ms = AccordionAccordion_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -31,3 +36,22 @@ instance Typeable ms => Pure AccordionAccordion ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (AccordionAccordion ms) where
+    type AsProp (AccordionAccordion ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a acc = acc { as = a }
+
+instance HasAttributesProp (AccordionAccordion ms) where
+    type Attribute (AccordionAccordion ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as a = a { attributes = as }
+
+instance HasChildrenProp (AccordionAccordion ms) where
+    type Child (AccordionAccordion ms) = View ms
+    getChildren = children
+    setChildren cs a = a { children = cs }
+
+instance HasClassesProp (AccordionAccordion ms) where
+    getClasses = classes
+    setClasses cs a = a { classes = cs }
