7ed9e8fb5f0319df6aa63b33c5a039b5976883c6
grumply <sean@grump.ly>
2018-01-06 08:46:27 -0600

    Implement SIdebarPusher properties. Closes #349.

--- src/Semantic/Modules/Sidebar/SidebarPusher.hs
+++ src/Semantic/Modules/Sidebar/SidebarPusher.hs
@@ -5,6 +5,12 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Dimmed
+
 data SidebarPusher ms = SidebarPusher_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
@@ -33,3 +39,26 @@ instance Typeable ms => Pure SidebarPusher ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (SidebarPusher ms) where
+    type AsProp (SidebarPusher ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs a sp = sp { as = a }
+
+instance HasAttributesProp (SidebarPusher ms) where
+    type Attribute (SidebarPusher ms) = Feature ms
+    getAttributes = attributes
+    setAttributes as sp = sp { attributes = as }
+
+instance HasChildrenProp (SidebarPusher ms) where
+    type Child (SidebarPusher ms) = View ms
+    getChildren = children
+    setChildren cs sp = sp { children = cs }
+
+instance HasClassesProp (SidebarPusher ms) where
+    getClasses = classes
+    setClasses cs sp = sp { classes = cs }
+
+instance HasDimmedProp (SidebarPusher ms) where
+    getDimmed = dimmed
+    setDimmed d sp = sp { dimmed = d }
