9d7d63f9f1ad84cdedef6f68e923d506cde5a74b
grumply <sean@grump.ly>
2018-01-09 09:00:24 -0600

    Implement DropdownMenu properties. Closes #311.

--- src/Semantic/Modules/Dropdown/DropdownMenu.hs
+++ src/Semantic/Modules/Dropdown/DropdownMenu.hs
@@ -5,6 +5,12 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Scrolling
+
 data DropdownMenu ms = DropdownMenu_ 
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms] 
@@ -33,3 +39,26 @@ instance Pure DropdownMenu ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (DropdownMenu ms) where
+    type AsProp (DropdownMenu ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs f dm = dm { as = f }
+
+instance HasAttributesProp (DropdownMenu ms) where
+    type Attribute (DropdownMenu ms) = Feature ms
+    getAttributes = attributes 
+    setAttributes cs dm = dm { attributes = cs }
+
+instance HasChildrenProp (DropdownMenu ms) where
+    type Child (DropdownMenu ms) = View ms
+    getChildren = children
+    setChildren cs dm = dm { children = cs }
+
+instance HasClassesProp (DropdownMenu ms) where
+    getClasses = classes
+    setClasses cs dm = dm { classes = cs }
+
+instance HasScrollingProp (DropdownMenu ms) where
+    getScrolling = scrolling
+    setScrolling s dm = dm { scrolling = s }
