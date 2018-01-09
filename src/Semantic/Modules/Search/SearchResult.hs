737964615002efbd4824b646cc36580a655f53bb
grumply <sean@grump.ly>
2018-01-09 11:29:34 -0600

    Simplify SearchResult for #340.

--- src/Semantic/Modules/Search/SearchResult.hs
+++ src/Semantic/Modules/Search/SearchResult.hs
@@ -8,29 +8,27 @@ import Semantic.Utils
 
 import Semantic.Properties.As
 import Semantic.Properties.Attributes
+import Semantic.Properties.Children
 import Semantic.Properties.Classes
 import Semantic.Properties.Active
 import Semantic.Properties.OnClick
-import Semantic.Properties.Renderer
-import Semantic.Properties.Value
 
-data SearchResult item ms = SearchResult_
+data SearchResult ms = SearchResult_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
+    , children :: [View ms]
     , classes :: [Txt]
     , active :: Bool
-    , item :: Maybe item
-    , onClick :: Maybe item -> Ef ms IO ()
-    , renderSearchResult :: SearchResult item ms -> [View ms]
+    , onClick :: Ef ms IO ()
     } deriving (Generic)
 
-instance Default (SearchResult item ms) where
+instance Default (SearchResult ms) where
     def = (G.to gdef) { as = Div }
 
-pattern SearchResult :: Typeable item => SearchResult item ms -> View ms
+pattern SearchResult :: SearchResult ms -> View ms
 pattern SearchResult sr = View sr
 
-instance Typeable item => Pure (SearchResult item) ms where
+instance Pure SearchResult ms where
     render sr@SearchResult_ {..} =
         let
             cs =
@@ -41,40 +39,35 @@ instance Typeable item => Pure (SearchResult item) ms where
         in
             as
                 ( mergeClasses $ ClassList cs 
-                : On "click" def (\_ -> return $ Just (onClick item))
+                : On "click" def (\_ -> return $ Just onClick)
                 : attributes
                 )
-                (renderSearchResult sr)
+                children
 
-instance HasAsProp (SearchResult item ms) where
-    type AsProp (SearchResult item ms) = [Feature ms] -> [View ms] -> View ms
+instance HasAsProp (SearchResult ms) where
+    type AsProp (SearchResult ms) = [Feature ms] -> [View ms] -> View ms
     getAs = as
     setAs f sr = sr { as = f }
 
-instance HasAttributesProp (SearchResult item ms) where
-    type Attribute (SearchResult item ms) = Feature ms
+instance HasAttributesProp (SearchResult ms) where
+    type Attribute (SearchResult ms) = Feature ms
     getAttributes = attributes 
     setAttributes cs sr = sr { attributes = cs }
 
-instance HasClassesProp (SearchResult item ms) where
+instance HasChildrenProp (SearchResult ms) where
+    type Child (SearchResult ms) = View ms
+    getChildren = children
+    setChildren cs sr = sr { children = cs }
+
+instance HasClassesProp (SearchResult ms) where
     getClasses = classes
     setClasses cs sr = sr { classes = cs }
 
-instance HasActiveProp (SearchResult item ms) where
+instance HasActiveProp (SearchResult ms) where
     getActive = active
     setActive a sr = sr { active = a }
 
-instance HasValueProp (SearchResult item ms) where
-    type ValueProp (SearchResult item ms) = Maybe item
-    getValue = item
-    setValue v sr = sr { item = v }
-
-instance HasOnClickProp (SearchResult item ms) where
-    type OnClickProp (SearchResult item ms) = Maybe item -> Ef ms IO ()
+instance HasOnClickProp (SearchResult ms) where
+    type OnClickProp (SearchResult ms) = Ef ms IO ()
     getOnClick = onClick
     setOnClick oc sr = sr { onClick = oc }
-
-instance HasRendererProp (SearchResult item ms) where
-    type RendererProp (SearchResult item ms) = SearchResult item ms -> [View ms]
-    getRenderer = renderSearchResult
-    setRenderer r sr = sr { renderSearchResult = r }
