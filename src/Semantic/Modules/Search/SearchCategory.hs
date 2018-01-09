5d6ae47b6d6e649af110500bb3bec4bebad7046e
grumply <sean@grump.ly>
2018-01-09 09:01:51 -0600

    Implement SearchCategory properties. Closes #339.

--- src/Semantic/Modules/Search/SearchCategory.hs
+++ src/Semantic/Modules/Search/SearchCategory.hs
@@ -6,6 +6,14 @@ import Pure.View hiding (name,active)
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+import Semantic.Properties.Active
+import Semantic.Properties.Name
+import Semantic.Properties.Results
+
 data SearchCategory result ms = SearchCategory_ 
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms] 
@@ -42,3 +50,39 @@ instance Typeable result => Pure (SearchCategory result) ms where
                 ( Div [ ClassList [ "name" ] ] (renderSearchCategory sc)
                 : children 
                 )
+
+instance HasAsProp (SearchCategory result ms) where
+    type AsProp (SearchCategory result ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs f sc = sc { as = f }
+
+instance HasAttributesProp (SearchCategory result ms) where
+    type Attribute (SearchCategory result ms) = Feature ms
+    getAttributes = attributes 
+    setAttributes cs sc = sc { attributes = cs }
+
+instance HasChildrenProp (SearchCategory result ms) where
+    type Child (SearchCategory result ms) = View ms
+    getChildren = children
+    setChildren cs sc = sc { children = cs }
+
+instance HasClassesProp (SearchCategory result ms) where
+    getClasses = classes
+    setClasses cs sc = sc { classes = cs }
+
+instance HasActiveProp (SearchCategory result ms) where
+    getActive = active
+    setActive a sc = sc { active = a }
+
+instance HasNameProp (SearchCategory result ms) where
+    getName = name
+    setName n sc = sc { name = n }
+
+instance HasResultsProp (SearchCategory result ms) where
+    type ResultsProp (SearchCategory result ms) = [result]
+    getResults = results
+    setResults rs sc = sc { results = rs }
+
+pattern RenderSearchCategory :: (SearchCategory result ms -> [View ms]) -> SearchCategory result ms -> SearchCategory result ms
+pattern RenderSearchCategory rsc sc <- (renderSearchCategory &&& id -> (rsc,sc)) where
+    RenderSearchCategory rsc sc = sc { renderSearchCategory = rsc }
