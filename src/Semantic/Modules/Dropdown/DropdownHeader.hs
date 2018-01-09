436ba77f1f95c2fef1bb5fdf3bd5957536bc0ca6
grumply <sean@grump.ly>
2018-01-09 08:59:18 -0600

    Implement DropdownHeader properties. Closes #307.

--- src/Semantic/Modules/Dropdown/DropdownHeader.hs
+++ src/Semantic/Modules/Dropdown/DropdownHeader.hs
@@ -5,6 +5,11 @@ import Pure.View
 
 import Semantic.Utils
 
+import Semantic.Properties.As
+import Semantic.Properties.Attributes
+import Semantic.Properties.Children
+import Semantic.Properties.Classes
+
 data DropdownHeader ms = DropdownHeader_ 
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms] 
@@ -31,3 +36,22 @@ instance Pure DropdownHeader ms where
                 : attributes
                 )
                 children
+
+instance HasAsProp (DropdownHeader ms) where
+    type AsProp (DropdownHeader ms) = [Feature ms] -> [View ms] -> View ms
+    getAs = as
+    setAs f dh = dh { as = f }
+
+instance HasAttributesProp (DropdownHeader ms) where
+    type Attribute (DropdownHeader ms) = Feature ms
+    getAttributes = attributes 
+    setAttributes cs dh = dh { attributes = cs }
+
+instance HasChildrenProp (DropdownHeader ms) where
+    type Child (DropdownHeader ms) = View ms
+    getChildren = children
+    setChildren cs dh = dh { children = cs }
+
+instance HasClassesProp (DropdownHeader ms) where
+    getClasses = classes
+    setClasses cs dh = dh { classes = cs }
