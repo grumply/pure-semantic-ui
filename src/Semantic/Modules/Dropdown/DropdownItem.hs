ec8500e2a334c9a7227f72ade0afb8b44c290bf0
grumply <sean@grump.ly>
2018-01-09 11:28:09 -0600

    Simplify DropdownItem for #308.

--- src/Semantic/Modules/Dropdown/DropdownItem.hs
+++ src/Semantic/Modules/Dropdown/DropdownItem.hs
@@ -7,33 +7,31 @@ import Semantic.Utils
 
 import Semantic.Properties.As
 import Semantic.Properties.Attributes
+import Semantic.Properties.Children
 import Semantic.Properties.Classes
 import Semantic.Properties.Active
 import Semantic.Properties.Disabled
 import Semantic.Properties.OnClick
-import Semantic.Properties.Renderer
 import Semantic.Properties.Selected
-import Semantic.Properties.Value
 
-data DropdownItem item ms = DropdownItem_
+data DropdownItem ms = DropdownItem_
     { as :: [Feature ms] -> [View ms] -> View ms
     , attributes :: [Feature ms]
+    , children :: [View ms]
     , classes :: [Txt]
     , active :: Bool
     , disabled :: Bool
-    , onClick :: Maybe item -> Ef ms IO ()
+    , onClick :: Ef ms IO ()
     , selected :: Bool
-    , item :: Maybe item
-    , renderDropdownItem :: Maybe item -> [View ms]
     } deriving (Generic)
 
-instance Default (DropdownItem item ms) where
+instance Default (DropdownItem ms) where
     def = (G.to gdef) { as = Div }
 
-pattern DropdownItem :: Typeable item => DropdownItem item ms -> View ms
+pattern DropdownItem :: DropdownItem ms -> View ms
 pattern DropdownItem di = View di
 
-instance Typeable item => Pure (DropdownItem item) ms where
+instance Pure (DropdownItem ) ms where
     render di@DropdownItem_ {..} =
         let
             cs = 
@@ -46,48 +44,43 @@ instance Typeable item => Pure (DropdownItem item) ms where
         in
             as
                 ( mergeClasses $ ClassList cs
-                : On "click" def (\_ -> return $ Just $ onClick item)
+                : On "click" def (\_ -> return $ Just onClick)
                 : Role "option"
                 : Attribute "aria-disabled" (disabled ? "true" $ "false")
                 : Attribute "aria-active" (active ? "true" $ "false")
                 : Attribute "aria-selected" (selected ? "true" $ "false")
                 : attributes
                 )
-                (renderDropdownItem item)
+                children
 
-instance HasAsProp (DropdownItem item ms) where
-    type AsProp (DropdownItem item ms) = [Feature ms] -> [View ms] -> View ms
+instance HasAsProp (DropdownItem ms) where
+    type AsProp (DropdownItem ms) = [Feature ms] -> [View ms] -> View ms
     getAs = as
     setAs f di = di { as = f }
 
-instance HasAttributesProp (DropdownItem item ms) where
-    type Attribute (DropdownItem item ms) = Feature ms
+instance HasAttributesProp (DropdownItem ms) where
+    type Attribute (DropdownItem ms) = Feature ms
     getAttributes = attributes 
     setAttributes cs di = di { attributes = cs }
 
-instance HasClassesProp (DropdownItem item ms) where
+instance HasChildrenProp (DropdownItem ms) where
+    type Child (DropdownItem ms) = View ms
+    getChildren = children
+    setChildren cs di = di { children = cs }
+
+instance HasClassesProp (DropdownItem ms) where
     getClasses = classes
     setClasses cs di = di { classes = cs }
 
-instance HasDisabledProp (DropdownItem item ms) where
+instance HasDisabledProp (DropdownItem ms) where
     getDisabled = disabled
     setDisabled d di = di { disabled = d }
 
-instance HasOnClickProp (DropdownItem item ms) where
-    type OnClickProp (DropdownItem item ms) = Maybe item -> Ef ms IO ()
+instance HasOnClickProp (DropdownItem ms) where
+    type OnClickProp (DropdownItem ms) = Ef ms IO ()
     getOnClick = onClick
     setOnClick oc di = di { onClick = oc }
 
-instance HasSelectedProp (DropdownItem item ms) where
+instance HasSelectedProp (DropdownItem ms) where
     getSelected = selected
     setSelected s di = di { selected = s }
-
-instance HasValueProp (DropdownItem item ms) where
-    type ValueProp (DropdownItem item ms) = Maybe item
-    getValue = item
-    setValue v di = di { item = v }
-
-instance HasRendererProp (DropdownItem item ms) where
-    type RendererProp (DropdownItem item ms) = Maybe item -> [View ms]
-    getRenderer = renderDropdownItem
-    setRenderer r di = di { renderDropdownItem = r }
