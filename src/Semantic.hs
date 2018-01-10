73228a8e9fdc8150fd2eff804e19df7cd768042a
grumply <sean@grump.ly>
2018-01-10 09:39:33 -0600

    Remove Pure.View exports. Closes #390.

--- src/Semantic.hs
+++ src/Semantic.hs
@@ -9,5 +9,3 @@ import Semantic.Modules as Export hiding (RS)
 import Semantic.Properties as Export
 import Semantic.Utils as Export hiding (Width(Width))
 import Semantic.Views as Export
-
-import Pure.View as Export hiding (color,name,Label,Button,(!),(%),Name,Size,Big,Small,Color,Disabled,Link,Empty,Hidden,one,two,Width,Scale,Widths,Text,Section,Input,Type,Error,Header,Sub,Value,focused,OnChange,OnClick,Ref,Form,OnSubmit,Success,Required,Reversed,Width,Menu,Header,Table,unit,Src,Styled,Checked,OnMouseDown,Proxy,OnClose,Offset,Visibility,Transition,pure,simple,getStyles,addClass,Tab,Progress,Percent,TextArea,OnInput,Rows,Direction,Embed,Autoplay,Placeholder,OnMouseEnter,Selected,OnKeyUp)
