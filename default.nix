1d9d167895150a73583a9fb3474273b8138b7a24
grumply <sean@grump.ly>
2018-12-01 10:45:00 -0600

    Fix default.nix dependencies

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, base, pure-cond, pure, pure-lifted, pure-txt, pure-uri, stdenv }:
+{ mkDerivation, base, pure-cond, pure-lifted, pure, pure-txt, pure-uri, stdenv }:
 mkDerivation {
   pname = "pure-semantic-ui";
   version = "0.7.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base pure-cond pure-core pure-default pure-events pure-html pure-lifted pure-styles pure-txt pure-uri ];
+  libraryHaskellDepends = [ base pure-cond pure-lifted pure pure-txt pure-uri ];
   license = stdenv.lib.licenses.bsd3;
 }
