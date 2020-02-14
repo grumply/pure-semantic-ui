0844f0dd3d8b65ef9cef4229463956798cf8f3c8
Sean Hickman <sean@grumply.com>
2020-02-13 23:35:41 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure-core, pure-cond, pure-lifted, pure, pure-txt, pure-uri, stdenv }:
 mkDerivation {
   pname = "pure-semantic-ui";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure-core pure-cond pure-lifted pure pure-txt pure-uri ];
   license = stdenv.lib.licenses.bsd3;
